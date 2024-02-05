df8fbd9d4d9870fe416275fb00ccbaa389be10d4
Muhammad Hassan <a.hassan0600@gmail.com>
2024-02-05 11:59:43 -0800

    Changed name from ProcrastiNot to ADHDone

--- app/screens/Homepage.js
+++ app/screens/Homepage.js
@@ -133,7 +133,7 @@ function Homepage(props){
                        <EntypoIcons name='menu' size={40} onPress={() => setMenuPanalVisible(true)}/>
                     </TouchableOpacity>
 
-                    <Text style={styles.textFont}>Procrastinot</Text>
+                    <Text style={styles.textFont}>ADHDone</Text>
 
                     <TouchableOpacity>
                        <FeatherIcons name='settings' size={40}/>
