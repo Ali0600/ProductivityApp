c2123dc5050ad3ac016c36fea09d770e3dc89102
Muhammad Hassan <a.hassan0600@gmail.com>
2026-04-30 04:14:38 -0700

    Added @expo/vector-icons and expo-font for SDK 54 icon rendering

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default ({ config }) => {
     expo: {
       name: "ADHD Habits",
       slug: "adhd-habits",
-      version: "1.0.29",
+      version: "1.0.30",
       orientation: "portrait",
       icon: "./app/assets/icon.png",
       userInterfaceStyle: "light",
@@ -55,7 +55,8 @@ export default ({ config }) => {
             color: "#ffffff",
             sounds: []
           }
-        ]
+        ],
+        "expo-font"
       ]
     }
   };
