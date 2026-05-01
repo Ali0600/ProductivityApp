49a6d4b849daf0559fa7a62ad2a6f05c258ea576
Muhammad Hassan <a.hassan0600@gmail.com>
2026-05-01 00:13:00 -0700

    updated version

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default ({ config }) => {
     expo: {
       name: "ADHD Habits",
       slug: "adhd-habits",
-      version: "1.0.33",
+      version: "1.0.34",
       orientation: "portrait",
       icon: "./app/assets/icon.png",
       userInterfaceStyle: "light",
